28bc679a6e94aa92f62cbb01236034cedfb91d86
Michon van Dooren <michon1992@gmail.com>
2019-01-31 16:24:09 +0100

    Try to apply the changes hunk by hunk
    This will decrease the amount of conflicts.

--- create-merged.sh
+++ create-merged.sh
@@ -29,13 +29,17 @@ for mod in mod*; do
 	[ "$(realpath "$modfile")" == "$(realpath "$TARGET")" ] && continue
 
 	# Skip file if it is blacklisted
-	if grep -Fx "$mod $FILENAME" ./_ignoredfiles; then
+	if grep -Fx "$mod $FILENAME" ./_ignoredfiles > /dev/null 2>&1; then
 		echo "> Skipping changes from $mod (blacklisted in _ignoredfiles)"
 		continue
 	fi
 
 	# Generate a patch and attempt to apply it
 	echo "> Trying to apply changes from $mod"
-	patch -p1 -u -f "$TARGET" <(diff -a -U3 "$ORIGINAL" "$modfile" | tee /tmp/patch)
+	for i in {1..10000}; do
+		diff -a -U5 "$ORIGINAL" "$modfile" | filterdiff --hunks="$i" > /tmp/patch
+		[ -s /tmp/patch ] || break
+		patch -p1 -u -f "$TARGET" < /tmp/patch
+	done
 	echo "$modfile" >> "$LOG"
 done
