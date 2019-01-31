597bf232228b29c814262ba629981aa6d4a2be05
Michon van Dooren <michon1992@gmail.com>
2019-01-31 16:54:41 +0100

    Fixed the already-applied detection
    Previously this only ran before increasing the fuzz factor, causing
    patches that had already been applied with fuzz to be applied again.

--- create-merged.sh
+++ create-merged.sh
@@ -27,6 +27,25 @@ mkdir -p "$(dirname "$TARGET")"
 cp "$ORIGINAL" "$TARGET"
 [ -f "$LOG" ] && rm "$LOG"
 
+makeHashCopy() {
+	hash="$(md5sum "$1" | cut -c1-32)"
+	cp "$1" "/tmp/_$(basename "$1")_$(basename "$ORIGINAL")_$hash"
+}
+
+doPatchCommand() {
+	input="$1"
+	shift
+
+	# If the hunk already seems to be applied, skip it
+	if [[ "$(patch --dry-run "$@" < "$input" 2>&1 || true)" == *'previously applied'* ]]; then
+		echo "Hunk #1 skipped (changes are already present)"
+		return 0
+	fi
+
+	# Try to apply
+	patch "$@" < "$input"
+}
+
 applyPatch() {
 	# Split the patch into hunks
 	for i in {1..10000}; do
@@ -34,6 +53,9 @@ applyPatch() {
 
 		# If the hunk is empty, we've processed all hunks for this patch, so stop the loop
 		[ -s /tmp/hunk ] || break
+
+		# Store hunk by hash
+		makeHashCopy /tmp/hunk
 
 		# Split the hunk into smaller hunks
 		for j in {1..10000}; do
@@ -44,6 +66,9 @@ applyPatch() {
 
 			# Fix the markings for the subhunk
 			rediff /tmp/subhunk_ > /tmp/subhunk
+
+			# Store subhunk by hash
+			makeHashCopy /tmp/subhunk
 
 			# Apply the subhunk
 			applyHunk /tmp/subhunk | sed "s/Hunk #1/Hunk #$i.$j/g"
@@ -63,21 +88,14 @@ applyHunk() {
 		cp "$overridepath" "$1"
 	fi
 
-	# If the hunk already seems to be applied, skip it
-	if [[ "$(patch -u --ignore-whitespace --dry-run "$TARGET" < "$1" 2>&1 || true)" == *'previously applied'* ]]; then
-		echo "Hunk #1 skipped (changes are already present)"
-		return 0
-	fi
-
 	# Try to apply the entire hunk
-	patch -u --ignore-whitespace "$TARGET" < "$1" && return 0
-	return 1
+	doPatchCommand "$1" -u --ignore-whitespace "$TARGET" && return 0
 
 	# This failed, so now try to apply it with more fuzz
 	fuzz=3 # fuzz factor. default is 2, so no point trying with less
 	while [ $fuzz -le $PATCH_MAX_FUZZ ]; do
 		echo "Attempting to apply with more fuzz ($fuzz)"
-		patch -u --ignore-whitespace "$TARGET" --fuzz=$fuzz < /tmp/partialhunk && return 0
+		doPatchCommand "$1" -u --ignore-whitespace "$TARGET" --fuzz=$fuzz && return 0
 		fuzz="$((fuzz + 1))"
 	done
 
