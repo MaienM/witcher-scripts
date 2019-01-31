715d5341fefe12966a5d3f16d4f2adb033dc2d84
Michon van Dooren <michon1992@gmail.com>
2019-01-31 16:26:21 +0100

    Avoid trying to apply the same hunk twice
    This is mainly relevant if mods include parts of other mods, in attempts
    to be compatible with those.

--- create-merged.sh
+++ create-merged.sh
@@ -29,17 +29,26 @@ for mod in mod*; do
 	[ "$(realpath "$modfile")" == "$(realpath "$TARGET")" ] && continue
 
 	# Skip file if it is blacklisted
-	if grep -Fx "$mod $FILENAME" ./_ignoredfiles > /dev/null 2>&1; then
-		echo "> Skipping changes from $mod (blacklisted in _ignoredfiles)"
-		continue
-	fi
+	# if grep -Fx "$mod $FILENAME" ./_ignoredfiles > /dev/null 2>&1; then
+	# 	echo "> Skipping changes from $mod (blacklisted in _ignoredfiles)"
+	# 	continue
+	# fi
 
-	# Generate a patch and attempt to apply it
+	# Generate a patch and attempt to apply it, hunk by hunk
 	echo "> Trying to apply changes from $mod"
+	diff -a -U4 "$TARGET" "$modfile" > /tmp/patch || true
 	for i in {1..10000}; do
-		diff -a -U5 "$ORIGINAL" "$modfile" | filterdiff --hunks="$i" > /tmp/patch
-		[ -s /tmp/patch ] || break
-		patch -p1 -u -f "$TARGET" < /tmp/patch
+		# Get the hunk
+		filterdiff --hunks="$i" < /tmp/patch > /tmp/hunk
+		# If the hunk is empty, we've processed all hunks for this patch, so stop the loop
+		[ -s /tmp/hunk ] || break
+		# If the hunk already seems to be applied, skip it
+		if patch -u --ignore-whitespace --dry-run < /tmp/hunk 2>&1 | grep 'previously applied' > /dev/null 2>&1; then
+			echo "Skipping already-applied hunk"
+			continue
+		fi
+		# Apply the hunk
+		patch -u --ignore-whitespace "$TARGET" < /tmp/hunk
 	done
 	echo "$modfile" >> "$LOG"
 done
